8a2098e62e4ac55cfcb2972a8b4122545cc93712
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-23 10:13:53 +0900

    use webpack and ts-loader to bundle server side files

---
+++ webpack.config.server.js
@@ -0,0 +1,34 @@
+// webpack.config.server.js
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+
+module.exports = {
+  name: 'server',
+  entry: {
+    server: path.resolve(__dirname, 'src/server/server.ts'),
+  },
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx'],
+  },
+  externals: [nodeExternals()],
+  target: 'node',
+  node: {
+    __dirname: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.server.json',
+        },
+      },
+    ],
+  },
+}
